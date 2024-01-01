071bfe19bdc73ef676d68c2a45ea2f48c16117ee
Intrum1 <skoropadskiy1979@gmail.com>
2024-01-01 13:21:16 +0100

    2

--- src/generic/2.ts
+++ src/generic/2.ts
@@ -8,16 +8,24 @@ type AllType = {
   name: string;
   position: number;
   color: string;
-  weight: number
-}
+  weight: number;
+};
 
-function compare (top, bottom): AllType {
+function compare<
+  T extends Pick<AllType, "name" | "color">,
+  U extends Pick<AllType, "position" | "weight">,
+>(top: T, bottom: U): AllType {
   return {
     name: top.name,
     color: top.color,
     position: bottom.position,
     weight: bottom.weight,
-  }
+  };
 }
+const obj1 = { name: "Object 1", position: 1, color: "red", weight: 10 };
+const obj2 = { name: "Object2", position: 2, color: "blue", weight: 20 };
+
+const result = compare(obj1, obj2);
+console.log(result);
 
 export {};
